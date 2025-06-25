82a53dc5df617bf5974622cd6b6fe39ddf9a7551
Dayana <dayanabright68@gmail.com>
2025-06-25 20:48:14 +0100

    initial commit

---
+++ tic-tac-toe/Player.cs
@@ -0,0 +1,16 @@
+﻿namespace tic_tac_toe
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public char Piece { get; }
+        public int Score { get; set; }
+        public Player() { }
+        public Player(string name, char piece, int score = 0)
+        {
+            Name = name;
+            Piece = piece;
+            Score = score;
+        }
+    }
+}
